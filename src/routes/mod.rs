6e7a4d925c9fa1654b5d7fcf858f210121e23a7a
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-06-13 21:56:35 +0500

    refactored routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,4 +2,25 @@ pub mod auth;
 pub mod health_check;
 pub mod posts;
 
+use actix_web::web::{self, get, post};
 pub use health_check::*;
+
+use self::{
+    auth::{login, logout, sign_up},
+    posts::list,
+};
+
+pub fn configure_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health_check", get().to(health_check));
+
+    cfg.service(
+        web::scope("/api")
+            .service(
+                web::scope("/auth")
+                    .route("/signup", post().to(sign_up))
+                    .route("/login", post().to(login))
+                    .route("/logout", post().to(logout)),
+            )
+            .service(web::scope("/posts").route("", get().to(list))),
+    );
+}
--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,9 @@
 use actix_session::CookieSession;
-use actix_web::{
-    dev::Server,
-    web::{self, post},
-    App, HttpServer,
-};
+use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use web::get;
 
-use crate::routes::{
-    auth::{login, logout, sign_up},
-    health_check,
-    posts::list,
-};
+use crate::routes::configure_routes;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
@@ -20,11 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
         App::new()
             .wrap(actix_web::middleware::Logger::default())
             .wrap(CookieSession::signed(&[0; 32]).secure(false))
-            .route("/health_check", get().to(health_check))
-            .route("/api/auth/signup", post().to(sign_up))
-            .route("/api/auth/login", post().to(login))
-            .route("/api/auth/logout", post().to(logout))
-            .route("/api/posts", get().to(list))
+            .configure(configure_routes)
             .app_data(db_pool.clone())
     })
     .listen(listener)?
